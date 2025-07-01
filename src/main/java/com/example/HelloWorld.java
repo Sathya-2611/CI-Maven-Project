e21abf2c2b275759bbec9bc8242ddc5cfd3b719e
Sathya-2611 <jabsathya26@gmail.com>
2025-07-01 23:27:47 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -3,5 +3,7 @@ package com.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins + Maven + GitHub integration!");
+        System.out.println("Modified code — triggered CI from GitHub!");
+
     }
 }
